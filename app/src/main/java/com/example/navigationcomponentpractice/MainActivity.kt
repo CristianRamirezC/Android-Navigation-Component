b59b26ecdf3f5b2532e04c15f7f5de1d258e24b8
CristianRamirezC <cristiancrc97@hotmail.com>
2022-07-27 11:54:49 -0500

    Up Button added to the AppBar

--- app/src/main/java/com/example/navigationcomponentpractice/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponentpractice/MainActivity.kt
@@ -2,18 +2,31 @@ package com.example.navigationcomponentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
-import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.navigationcomponentpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.findNavController()
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController: NavController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
 
